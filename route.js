ef069f4495b9b1d0c05a9616b09d887d7c3cf384
camilanunezz <djs8zw@virginia.edu>
2024-08-07 22:56:11 -0400

    Trying to connect api key

--- route.js
+++ route.js
@@ -1,6 +1,9 @@
 //API route will handle communication between frontend and OpenAI API
 import { NextResponse } from 'next/server';
+
 import Groq from 'groq-sdk';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
